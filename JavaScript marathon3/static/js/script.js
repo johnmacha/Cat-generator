e0cb5d74089539ad67914474e8559c2f97c92048
johnmacha <64866022+johnmacha@users.noreply.github.com>
2023-04-13 11:36:04 +0300

    Add files via upload

---
+++ JavaScript marathon3/static/js/script.js
@@ -0,0 +1,7 @@
+
+function generateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "static/kitty-cat-sandwich.gif";
+    div.appendChild(image);
+}
